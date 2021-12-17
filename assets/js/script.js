da829f811e1a674cf2cdc40782289a0d065abf98
mailahnheidi@gmail.com <mailahnheidi@gmail.com>
2021-12-17 14:14:54 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,16 +55,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
-   
+   $(this).addClass("dropover");
+   $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event, ui) {
-
+   $(this).removeClass("dropover");
+   $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
- 
+   $(event.target).addClass("dropover-active");
   },
   out: function(event) {
- 
+   $(event.target).removeClass("dropover-active");
   },
   update: function() {
     var tempArr = [];
@@ -107,10 +109,10 @@ $("#trash").droppable({
   drop: function(event, ui) {
     // remove dragged element from the dom
     ui.draggable.remove();
-
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
-  
+  $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
 
@@ -131,7 +133,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
